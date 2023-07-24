ad491766db3fa30a282b6c763ec62d5f4010f7db
babulalt <babulal.tamang@berrybytes.com>
2023-07-24 22:30:49 +0545

    Student document model updated

--- api/models/StudentFile.go
+++ api/models/StudentFile.go
@@ -4,6 +4,7 @@ import "github.com/jinzhu/gorm"
 
 type StudentFile struct {
 	gorm.Model
+	Photo                 string  `json:"photo"`
 	SEETranscript         string  `json:"see_transcript"`
 	SEECharacter          string  `json:"see_character"`
 	CertifiacteTranscript string  `json:"certificate_transcript"`
@@ -11,6 +12,7 @@ type StudentFile struct {
 	CertificateMigration  string  `json:"certificate_migration"`
 	CitizenshipFront      string  `json:"citizenship_front"`
 	CitizenshipBack       string  `json:"citizenship_back"`
+	Signature             string  `json:"signature"`
 	StudentID             uint    `json:"student_id"`
 	Student               Student `json:"student"`
 }
